62fa6c86799e8da85db62be1f7f84828fc4024a6
Babak <111429093+BabakStars@users.noreply.github.com>
2023-02-13 12:49:58 +0330

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+import cv2 as cv
+img1 = cv.imread('D:/youtube/General/61570421.jpg',0)
+img2 = cv.imread('D:/youtube/General/61570421.jpg',1)
+img3 = cv.imread('D:/youtube/General/61570421.jpg',-1)
+
+#cv.IMREAD_COLOR                1
+#cv.IMREAD_GRAYSCALE            0
+#cv.IMREAD_IGNORE_ORIENTATION   -1
+
+cv.imshow('IM1',img1)
+cv.imshow('IM2',img2)
+cv.imshow('IM3',img3)
+cv.waitKey(0)
+cv.destroyAllWindows()
